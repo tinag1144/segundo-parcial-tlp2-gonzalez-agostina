2b8a35558e9a2d7e17031dda70483b5ae287b7b3
unknown <sogazelaznog@gmail.com>
2025-11-14 15:27:37 -0300

    Se realizó la lógica para obtener datos del usuario desde la api. Se implementó la función handleLogout usando credentials: 'true'. Se hizo el redireccionamiento

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,70 @@
+import { useState } from "react";
+
 export const Navbar = () => {
   // TODO: Obtener datos del usuario desde /api/profile
   // TODO: Implementar función handleLogout con POST a /api/logout usando credentials: 'include'
   // TODO: Después del logout exitoso, redireccionar a /login
   // TODO: Manejar errores apropiadamente
 
-  const userName = "Usuario"; // TODO: Reemplazar con el nombre real del usuario obtenido de /api/profile
+  //primero inicializo el estado de la autenticacion del usuario (empieza en false asumiendo que no se autenticó aún)
+  const [authenticated, setIsAuthenticated] = useState(false);
+
+  //estado para guardar el nombre del usuario desde el api/profile 
+  const [username, setUserName] = useState("")
+
+  //este useEffect se va a ejecutar cada vez que el usuario vaya a una nueva URL (location), esto es para que se verifique el estado de la autenticación en cada cambio de url 
+  useEffect(() => {
+    //función para verificar el perfil 
+    const checkProfile = async () => {
+      try {
+        //se hace una petición GET a la API 
+        const res = await fetch("http://localhost:3000/api/profile", {
+          method: "GET",
+          credentials: "include", //esto le dice al navegador que envie las cookies con la petición 
+        });
+
+        if (!res.ok) {
+          setIsAuthenticated(false);
+          return;
+        }
+
+        const data = await res.json();
+
+        
+
+         // Si la petición funcionó, ya está autenticado
+        setIsAuthenticated(true);
+
+      } catch (error) {
+        console.error("Error verificando perfil:", error);
+        setIsAuthenticated(false);
+      }
+    };
+    //llamo a la función 
+    checkProfile();
+  }, []); //se ejecuta solo una vez, cuando carga el navbar 
+
+  //LOGOUT
+  const handleLogout = async () => {
+    try {
+      const res = await fetch("http://localhost:3000/api/logout", {
+        method: "POST",
+        credentials: "include",
+      });
+
+      if (res.ok) {
+        setIsAuthenticated(false);
+        navigate("/login");
+      } else {
+        alert("No se pudo cerrar sesión");
+      }
+    } catch (error) {
+      console.error("Error al cerrar sesión:", error);
+      alert("Error cerrando sesión");
+    }
+  };
+
+  const userName = setUserName(data.username); // TODO: Reemplazar con el nombre real del usuario obtenido de /api/profile
 
   return (
     <nav className="bg-gray-900 text-white h-16 left-0 right-0 shadow-lg sticky top-0 z-50">
@@ -19,7 +79,8 @@ export const Navbar = () => {
 
           <button
             onClick={() => {
-              // TODO: Implementar handleLogout aquí
+              handleLogout
+               //TODO: Implementar handleLogout aquí
             }}
             className="bg-red-600 hover:bg-red-700 px-4 py-2 rounded transition-colors font-medium"
           >
